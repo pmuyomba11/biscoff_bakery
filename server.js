ea5e1b682d94f93e061a2ff26d078c70b53365fd
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-09-14 18:40:16 -0400

    server setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config();
+const colors = require('colors');
+const morgan = require('morgan');
+const PORT = process.env.PORT;
+//Initialization....
+const app = express();
+
+//Middleware...
+app.use(express.urlencoded({extended : true}));
+app.use(morgan('dev'));
+
+//Routes..
+
+
+//Listener...
+app.listen(PORT, () => {
+    console.log('====================================');
+    console.log(`Server running on PORT: ${PORT}...`.inverse.blue);
+    console.log('====================================');
+})
