41cc7a9a0195234cade10b057d4fc8d8181eb400
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-09-14 19:43:15 -0400

    views update

--- server.js
+++ server.js
@@ -7,13 +7,34 @@ const PORT = process.env.PORT;
 //Initialization....
 const app = express();
 
+
 //Middleware...
 app.use(express.urlencoded({extended : true}));
 app.use(morgan('dev'));
 
 //Routes..
+//Index Route
+app.get('/products', (req, res) => {
+    res.render('index.ejs', {
+        allProducts: products
+    });
+})
+//New Route
+app.get('/products/new', (req, res) => {
+    res.render('new.ejs')
+})
 
-
+//Create route
+app.post('/products', (req, res) => {
+    products.push(req.body);
+    res.redirect('/products')
+})
+//Show Route
+app.get('/products/:id', (req, res) => {
+    res.render('show.ejs', {
+        product: products[req.params.id]
+    })
+})
 //Listener...
 app.listen(PORT, () => {
     console.log('====================================');
